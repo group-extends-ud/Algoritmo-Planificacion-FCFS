019703a43eae4388c6c8786fde10512cd866956d
spaezsuarez <spaezsuarez@gmail.com>
2022-02-11 20:58:12 -0500

    Declaration of first hook

---
+++ src/hooks/FirstComeSolver.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ProcessModel } from 'models/ProcessModel';
+
+export const usePlanificationSolver = (incomingProcess:ProcessModel[]):void => {
+
+    incomingProcess.forEach((process:ProcessModel) => {
+        
+
+    });
+
+
+}
